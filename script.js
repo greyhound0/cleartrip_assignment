b9d8c8027d2f7bedb15b167ca90af968d08e07a2
manas gupta <manasgupta0110@gmail.com>
2023-06-06 22:37:45 +0530

    final changes

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const iconContainer = document.querySelectorAll(".iconContainer");
 const icon = document.querySelectorAll(".icon");
+const button = document.getElementById("loginButton");
+button.addEventListener("click", function () {
+  button.classList.add("clicked");
+  setTimeout(function () {
+    button.classList.remove("clicked");
+  }, 300);
+});
 
 function changeColor(element) {
   if (element.style.color === "rgb(37, 110, 227)") {
