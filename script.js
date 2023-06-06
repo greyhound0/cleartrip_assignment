6bfc9549d995d6f1e9225a1ba449f368a998eddd
manas gupta <manasgupta0110@gmail.com>
2023-06-06 21:12:29 +0530

    basic ui done

---
+++ script.js
@@ -0,0 +1,10 @@
+const iconContainer = document.querySelectorAll(".iconContainer");
+const icon = document.querySelectorAll(".icon");
+
+function changeColor(element) {
+  if (element.style.color === "rgb(37, 110, 227)") {
+    element.style.color = "black";
+  } else {
+    element.style.color = "rgb(37, 110, 227)";
+  }
+}
